ad3eb3751c44db721995464a19f9918019545097
Geoff Dudgeon <dudgeon@gmail.com>
2015-09-26 13:54:08 -0400

    Initial Commit

---
+++ ParseSwiftPlaygroundTests/ParseSwiftPlaygroundTests.swift
@@ -0,0 +1,36 @@
+//
+//  ParseSwiftPlaygroundTests.swift
+//  ParseSwiftPlaygroundTests
+//
+//  Created by Geoffrey Dudgeon on 9/26/15.
+//  Copyright © 2015 Geoff Dudgeon. All rights reserved.
+//
+
+import XCTest
+@testable import ParseSwiftPlayground
+
+class ParseSwiftPlaygroundTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
